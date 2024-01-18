2dc1fceffc196e804772d5b9a65b6180c0a99114
Marius Topircean <mtopircean@yahoo.com>
2024-01-18 20:19:07 +0000

    Create profile details view with get and put methods

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,35 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.http import Http404
+
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+    
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+        except:
+            raise Http404
+        
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
