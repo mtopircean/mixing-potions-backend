3bfd1149b37d1e8d87a906c080885180888b5c29
Marius Topircean <mtopircean@yahoo.com>
2024-01-19 20:49:40 +0000

    Update Product Serializer to display name only using slug for Condition and BodySystem

--- products/serializers.py
+++ products/serializers.py
@@ -1,11 +1,28 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Condition, BodySystem
+
+class ConditionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Condition
+        fields = '__all__'
+
+class BodySystemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BodySystem
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    condition = serializers.StringRelatedField(many=True)
-    body_systems = serializers.StringRelatedField(many=True)
-    
+    condition = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name',
+        queryset=Condition.objects.all()
+    )
+    body_systems = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name',
+        queryset=BodySystem.objects.all()
+    )
     class Meta:
         
         model = Product
