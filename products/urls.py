0ec265d74eeeec94420b2a72e1b3a4a6f001f65d
Marius Topircean <mtopircean@yahoo.com>
2024-01-19 20:10:05 +0000

    Imported permissions and set permission classed to is admin in order to access put for product

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from products import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -5,6 +5,7 @@ from .models import Product
 from .serializers import ProductSerializer
 from django.http import Http404
 from mixing_potions_api.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class ProductList(APIView):
@@ -12,3 +13,30 @@ class ProductList(APIView):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+    
+
+class ProductDetail(APIView):
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
+    def get_object(self, pk):
+        try:
+            product = Product.objects.get(pk=pk)
+            self.check_object_permissions(self.request, product)
+            return product
+        except:
+            raise Http404
+        
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
