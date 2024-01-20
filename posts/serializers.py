4275d64c12289c4af5c353aa04f428b840e76047
Marius Topircean <mtopircean@yahoo.com>
2024-01-20 10:35:23 +0000

    Update PostSerializer to include product details and user nickname

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
 from .models import Post
+from products.serializers import ProductSerializer
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     owner_nickname = serializers.SerializerMethodField()
+    products = ProductSerializer(many=True, read_only=True)
     
     class Meta:
         
         model = Post
         fields = [
-            'id', 'title', 'description', 'created_at', 'updated_at', 'products',
-            'image', 'owner', 'owner_nickname',
+            'id', 'title', 'description', 'created_at', 'updated_at',
+            'image', 'owner', 'owner_nickname', 'products',
         ]
         
     def get_owner_nickname(self, obj):
