87e0ac9c33a96a95decbfe6ac32089e9c2ec29aa
Marius Topircean <mtopircean@yahoo.com>
2024-01-27 15:52:26 +0000

    Amdd pagination, date/time format and naturaltime to comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 from django.contrib.auth.models import User
@@ -19,11 +20,19 @@ class CommentSerializer(serializers.ModelSerializer):
         read_only=True,
         view_name='post-detail',
         source='post'
-    ) 
+    )
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
     
     def get_is_owner(self, obj):
         request = self.context.get('request')
         return request and request.user == obj.owner
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
     
     class Meta:
         model = Comment
