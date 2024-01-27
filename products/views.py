e8494acedf4096422092b5775a3c8695f2dba2de
Marius Topircean <mtopircean@yahoo.com>
2024-01-27 09:01:31 +0000

    Refactor product list and detail, gave also create permission in list for admin

--- products/views.py
+++ products/views.py
@@ -1,42 +1,18 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
-from django.http import Http404
-from mixing_potions_api.permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from mixing_potions_api.permissions import IsOwnerOrReadOnly
 
 
-class ProductList(APIView):
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-    
 
-class ProductDetail(APIView):
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
     
-    def get_object(self, pk):
-        try:
-            product = Product.objects.get(pk=pk)
-            self.check_object_permissions(self.request, product)
-            return product
-        except:
-            raise Http404
-        
-    def get(self, request, pk):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        product = self.get_object(pk)
-        
-        serializer = ProductSerializer(product, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProductDetail(generics.RetrieveUpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    lookup_field = 'pk'
