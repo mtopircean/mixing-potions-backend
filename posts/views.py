ac8c1f580eeedbb45d56a7a8c72e785a52d3098f
Marius Topircean <mtopircean@yahoo.com>
2024-01-27 12:52:30 +0000

    Create filter and search for posts

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from rest_framework.filters import OrderingFilter, SearchFilter
 from .models import Post
 from .serializers import PostSerializer
 from mixing_potions_api.permissions import IsOwnerOrReadOnly
@@ -8,6 +9,14 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    
+    filter_backends = [
+        OrderingFilter,
+        SearchFilter
+        ]
+    ordering_fields = ['title', 'created_at', 'updated_at',
+                      'owner__profile__nickname']
+    search_fields = ['title', 'owner__profile__nickname', 'products__name']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
