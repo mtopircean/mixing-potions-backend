d0240b6a20899e94586c3725eb9c1e9c7d052651
Marius Topircean <mtopircean@yahoo.com>
2024-01-27 11:22:15 +0000

    Add followed and following functionality to Profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
+from django.contrib.auth.models import User
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    followers = serializers.SerializerMethodField()
+    following = serializers.SerializerMethodField()
     
     class Meta:
         
@@ -10,6 +14,33 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'user_status', 'owner', 'first_name', 'last_name', 
             'age', 'about', 'nickname', 'email', 'phone_number', 
-            'member_since', 'updated_at', 'image'
+            'member_since', 'updated_at', 'image', 'followers',
+            'following',
         ]
         read_only_fields = ['user_status']
+        
+    def get_followers(self, obj):
+        followers = Follower.objects.filter(followed=obj.owner)
+        follower_nicknames = []
+
+        for follower in followers:
+            profile = Profile.objects.filter(owner=follower.owner).first()
+            if profile and profile.nickname:
+                follower_nicknames.append(profile.nickname)
+            else:
+                follower_nicknames.append(follower.owner.username)
+
+        return follower_nicknames
+    
+    def get_following(self, obj):
+        following = Follower.objects.filter(owner=obj.owner)
+        following_nicknames = []
+
+        for followed_user in following:
+            profile = Profile.objects.filter(owner=followed_user.followed).first()
+            if profile and profile.nickname:
+                following_nicknames.append(profile.nickname)
+            else:
+                following_nicknames.append(followed_user.followed.username)
+
+        return following_nicknames
