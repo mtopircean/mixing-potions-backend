da1dcf0587290e5b24b33ac43c82f49865bff16b
Marius Topircean <mtopircean@yahoo.com>
2024-01-27 09:24:00 +0000

    Update followers serializer to display user nickname

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework import serializers
 from .models import Follower
 from django.contrib.auth.models import User
+from profiles.models import Profile
 
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     followed_name = serializers.ReadOnlyField(source='followed.username')
+    followed_nickname = serializers.SerializerMethodField()
     
     class Meta:   
         model = Follower
-        fields = ['id', 'owner', 'followed', 'followed_name', 'created_at']
+        fields = ['id', 'owner', 'followed', 'followed_name', 'followed_nickname', 'created_at']
+        
+    def get_followed_nickname(self, obj):
+        profile = Profile.objects.filter(owner=obj.followed).first()
+        return profile.nickname if profile else None
         
     def validate(self, data):
         owner_id = self.context['request'].user.id
