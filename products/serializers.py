b5a3703696b103ffe1766e4c33a8bb42bf0bc364
Marius Topircean <mtopircean@yahoo.com>
2024-01-27 11:57:17 +0000

    Add functionality to products to display where used and by whom

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,17 @@
 from rest_framework import serializers
+from posts.models import Post
+from profiles.models import Profile
 from .models import Product, Condition, BodySystem
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title']
+        
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['nickname']
 
 class ConditionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,8 +35,21 @@ class ProductSerializer(serializers.ModelSerializer):
         slug_field='name',
         queryset=BodySystem.objects.all()
     )
+    posts_used_in = PostSerializer(many=True, read_only=True, source='get_posts_used_in')
+    used_by_users = ProfileSerializer(many=True, read_only=True, source='get_used_by_users')
+    
     class Meta:
         
         model = Product
         fields = '__all__'
         read_only_fields = ['user_status']
+        
+    def get_posts_used_in(self,obj):
+        posts = Post.objects.filter(products=obj)
+        post_serializer = PostSerializer(posts, many=True)
+        return post_serializer.data
+    
+    def get_used_by_users(self, obj):
+        users = Profile.objects.filter(products_used=obj)
+        profile_serializer = ProfileSerializer(users, many=True)
+        return profile_serializer.data
