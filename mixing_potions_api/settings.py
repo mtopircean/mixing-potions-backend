84f2a3ea38fc9739834ff23e9828e7ea3fd1dcd6
Marius Topircean <mtopircean@yahoo.com>
2024-02-24 10:26:37 +0000

    Revert settings to use production env databse

--- mixing_potions_api/settings.py
+++ mixing_potions_api/settings.py
@@ -125,16 +125,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mixing_potions_api.wsgi.application'
 
 # if 'DEV' in os.environ:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# else:
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+# else:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
