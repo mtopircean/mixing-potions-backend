5068e34fb2bdcc530a15daabbe007bd83bfa92c7
Marius Topircean <mtopircean@yahoo.com>
2024-02-24 10:23:20 +0000

    Update unittest for comments to align to comments models fields

--- comments/tests.py
+++ comments/tests.py
@@ -14,24 +14,21 @@ class CommentAPITests(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-
     def test_comment_list(self):
         response = self.client.get('/comments/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
     def test_create_comment(self):
-        post = Post.objects.create(title='Test Post', content='Test Content', owner=self.user)
-        data = {'content': 'Test Comment', 'post': post.pk}
+        post = Post.objects.create(
+            title='Test Post', description='Test description', owner=self.user)
+        data = {'comment_text': 'Test Comment', 'post': post.pk}
         response = self.client.post('/comments/', data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Comment.objects.count(), 1)
-    
-    
+
     def test_delete_comment(self):
-        post = Post.objects.create(title='Test Post', content='Test Content', owner=self.user)
-        comment = Comment.objects.create(owner=self.user, content='Test Comment', post=post)
+        post = Post.objects.create(
+            title='Test Post', description='Test description', owner=self.user)
+        comment = Comment.objects.create(
+            owner=self.user, comment_text='Test Comment', post=post)
         response = self.client.delete(f'/comments/{comment.pk}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Comment.objects.count(), 0)
-    
