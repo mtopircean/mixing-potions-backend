b4133973cb7741a06bb0712cf5d6590a50c3474b
Marius Topircean <mtopircean@yahoo.com>
2024-02-24 08:35:26 +0000

    Add Unittest for product app, retrieve and update products

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your tests here.
+
+class ProductAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='admin',
+                                             password='1234',
+                                             is_staff=True)
+        self.client.force_authenticate(user=self.user)
+        self.product = Product.objects.create(name='Product',
+                                              condition='Headache',
+                                              body_systems='BodySystem1,BodySystem2')
+
+    def test_retrieve_product(self):
+        response = self.client.get(f'/products/{self.product.pk}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_product(self):
+        new_name = 'New Name'
+        response = self.client.patch(f'/products/{self.product.pk}/',
+                                     {'name': new_name})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.product.refresh_from_db()
+        self.assertEqual(self.product.name, new_name)
